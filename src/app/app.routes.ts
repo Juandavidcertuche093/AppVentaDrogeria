d5e73856150c862087f0511193ae69fd59a175c8
juandavidcertuche93 <juandavidcertuchequijano2@gmail.com>
2024-11-13 16:22:59 -0500

    - Agregar formulario y funcionalidad de inicio de sesión. - Configurar rutas de autenticación. - Crear modelos para el inicio de sesión y las respuestas de API. - Actualizar la configuración y el estilo de la aplicación.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',//ruta principal o ruta publica
+        // canActivate: [redirectGuard],
+        loadChildren: () => import('./modules/auth/auth.module').then((m) => m.AuthModule)
+    },
+];
---
+++ src/app/modules/auth/components/login-form/login-form.component.ts
@@ -0,0 +1,74 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { faPen, faEye, faEyeSlash, faUser, faUserCircle, faUserMd } from '@fortawesome/free-solid-svg-icons';
+import { Router } from '@angular/router';
+import { UsuarioService } from '../../services/usuario.service';
+import { UtilidadService } from '../../../../services/utilidad.service';
+import { Login } from '../../../../core/models/login';
+
+@Component({
+  selector: 'app-login-form',
+  templateUrl: './login-form.component.html'
+})
+export class LoginFormComponent {
+
+  formularioLogin = this.formBuilder.nonNullable.group({
+    email:['',[Validators.required, Validators.email]],
+    password:['',[Validators.required, Validators.minLength(5)]]
+  });
+
+  faPen = faPen;
+  faEye = faEye;
+  faUser = faUser
+  faEyeSlash = faEyeSlash;
+  faUserCircle = faUserCircle;
+  faUserMd = faUserMd 
+  showPassword = false;
+  mostrarLoading:boolean=false;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private usuarioServicio: UsuarioService,
+    private utilidadServicio: UtilidadService
+  ){}
+
+  iniciarSesion(){
+    this.mostrarLoading = true;
+    const request: Login = {
+      correo: this.formularioLogin.value.email ?? '',
+      clave: this.formularioLogin.value.password ?? ''
+    }
+    this.usuarioServicio.iniciarSesion(request)
+    .subscribe({
+      next: (data) => {
+        if (data.status) {
+          const sesion = {
+            idUsuario: data.value.idUsuario,
+            correo: data.value.correo,
+            rolDescripcion: data.value.rolDescripcion
+          };
+          this.utilidadServicio.guardarSesionUsuario(sesion)//solo se guarda la parate no sencible de los datos
+          // Verificar el rol del usuario y redirigirlo
+          if (sesion.rolDescripcion === 'Administrador') {
+            this.router.navigate(['/dashboard']); // Redirige al dashboard si es Administrador
+          } else if (sesion.rolDescripcion === 'Empleado') {
+            this.router.navigate(['/venta']); // Redirige a venta si es Empleado
+          } else {
+            this.router.navigate(['/401']); // Si el rol no es válido, redirige a una página no autorizada
+          }
+        } else {
+          this.utilidadServicio.mostrarAlerta("Nombre o contraseñas incorrectas. Por favor, verifique tus credenciales e intentelo nuevamente", "Opps")
+        }
+      },
+      complete: () => {
+        this.mostrarLoading = false;
+      },
+      error: () => {
+        this.mostrarLoading = false;// Detener el loading en caso de error
+        this.utilidadServicio.mostrarAlerta("Hubo un error con la conexión al servidor intentalo nuevamente mas tarde", "Opps");
+      }
+    });
+  }
+
+}
