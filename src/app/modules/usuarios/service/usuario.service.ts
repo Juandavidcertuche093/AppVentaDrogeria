ba360bcbb9d3f1cd31324779ec1984bbd2289f4a
juandavidcertuche93 <juandavidcertuchequijano2@gmail.com>
2024-11-14 10:44:24 -0500

    Agregar modelo y servicios (rol y usuario) para el módulo de usuarios

---
+++ src/app/modules/usuarios/service/usuario.service.ts
@@ -0,0 +1,34 @@
+import { inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable} from 'rxjs';
+import { environment } from '../../../../environments/environments';
+import {Usuario} from '../../../core/models/usuario';
+import { ResponseApi } from '../../../core/models/response-api';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsuarioService {
+
+  private urlApi:string = environment.API_URL + "Usuario/";
+
+  private http = inject(HttpClient)
+
+  constructor() { }
+
+  lista():Observable<ResponseApi>{
+    return this.http.get<ResponseApi>(`${this.urlApi}Lista`)
+  }
+
+  guardar(resquest: Usuario):Observable<ResponseApi>{
+    return this.http.post<ResponseApi>(`${this.urlApi}Guardar`,resquest)
+  }
+
+  editar(request: Usuario):Observable<ResponseApi>{
+    return this.http.put<ResponseApi>(`${this.urlApi}Editar`, request)
+  }
+
+  eliminar(id: number):Observable<ResponseApi>{
+    return this.http.delete<ResponseApi>(`${this.urlApi}Eliminar/${id}`)
+  }
+}
